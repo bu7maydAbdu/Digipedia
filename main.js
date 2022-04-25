067d64cb9c91c94728f8b2425fa3c826b609ab11
mohammed abdulrahman idrees <71595830+bu7maydAbdu@users.noreply.github.com>
2022-04-25 18:11:53 +0300

    organized the style of the page and divided it into two sections one for searching and the other to display results

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
-const url = `https://digimon-api.vercel.app/api/digimon/name/agumon`
-fetch(url)
-      .then(res => res.json()) // parse response as JSON
-      .then(data => {
-        console.log(data)
-        document.querySelector("h2").innerText = data[0].name
-        document.querySelector("h3").innerText = data[0].level
-        document.querySelector("img").src = data[0].img
-      })
-      .catch(err => {
-          console.log(`error ${err}`)
-      });
+const getButton = document.querySelector("button")
+getButton.addEventListener("click", getFetch)
+
+function getFetch(){
+
+    const digiName = document.querySelector("input").value
+
+    const url = `https://digimon-api.vercel.app/api/digimon/name/${digiName}`
+    fetch(url)
+          .then(res => res.json()) // parse response as JSON
+          .then(data => {
+            console.log(data)
+            document.querySelector("h2").innerText = data[0].name
+            document.querySelector("h3").innerText = data[0].level
+            document.querySelector("img").src = data[0].img
+          })
+          .catch(err => {
+              console.log(`error ${err}`)
+          });
+
+
+}
+
+
